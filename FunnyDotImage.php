1ad8de4f9a6fa3875722b5f4a42a59d80bd356ac
Pierre Schmitz <pierre@archlinux.de>
2007-06-25 17:39:04 +0200

    benutze statisches FunnyDotImage

--- FunnyDotImage.php
+++ FunnyDotImage.php
@@ -7,22 +7,13 @@ $time = time();
 setCookie('AntiSpamTime', $time);
 setCookie('AntiSpamHash', sha1($time.$wgAntiSpamHash));
 
-$im = imagecreatetruecolor(1, 1);
-
-ob_start();
-
 header('HTTP/1.1 200 OK');
 header("Cache-Control: no-cache, must-revalidate");
 header('Content-Type: image/png');
-header('Content-Length: '.ob_get_length());
+header('Content-Length: 69');
 
-imagepng($im);
-imagedestroy($im);
-
-while (ob_get_level() > 0)
-	{
-	ob_end_flush();
-	}
+/** transparent png (1px*1px) */
+echo base64_decode('iVBORw0KGgoAAAANSUhEUgAAAAEAAAABCAIAAACQd1PeAAAADElEQVQImWNgYGAAAAAEAAGjChXjAAAAAElFTkSuQmCC');
 
 exit;
 
