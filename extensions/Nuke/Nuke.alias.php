0e8a0afedebf2b6df9f70e0ffe3d74765417b86e
Pierre Schmitz <pierre@archlinux.de>
2010-09-14 09:06:35 +0200

    Add Nuke extension
    This extension allow mass deletion of pages
    Revision: 72823
    Homepage: http://www.mediawiki.org/wiki/Extension:Nuke

---
+++ extensions/Nuke/Nuke.alias.php
@@ -0,0 +1,152 @@
+<?php
+/**
+ * Aliases for Special:Nuke
+ *
+ * @file
+ * @ingroup Extensions
+ */
+
+$aliases = array();
+
+/** English
+ * @author Brion Vibber
+ */
+$aliases['en'] = array(
+	'Nuke' => array( 'Nuke' ),
+);
+
+$aliases['ar'] = array(
+	'Nuke'   => array( 'حذف_كمي' ),
+);
+
+$aliases['arz'] = array(
+	'Nuke'   => array( 'مسح_كامل' ),
+);
+
+$aliases['be-tarask'] = array(
+	'Nuke'   => array( 'Масавае_выдаленьне' ),
+);
+
+$aliases['de'] = array(
+	'Nuke'   => array( 'Massenlöschung' ),
+);
+
+$aliases['dsb'] = array(
+	'Nuke'   => array( 'Wulašowanje_z_kopicami' ),
+);
+
+$aliases['et'] = array(
+	'Nuke'   => array( 'Lauskustutamine' ),
+);
+
+$aliases['fa'] = array(
+	'Nuke'   => array( 'حذف_دسته‌جمعی' ),
+);
+
+$aliases['fi'] = array(
+	'Nuke'   => array( 'Massapoisto' ),
+);
+
+$aliases['gsw'] = array(
+	'Nuke'   => array( 'Masseleschig' ),
+);
+
+$aliases['haw'] = array(
+	'Nuke'   => array( 'Luku' ),
+);
+
+$aliases['he'] = array(
+	'Nuke'   => array( 'מחיקה_מרובה' ),
+);
+
+$aliases['hsb'] = array(
+	'Nuke'   => array( 'Masowe_wušmórnjenje' ),
+);
+
+$aliases['hu'] = array(
+	'Nuke'   => array( 'Kiirtás' ),
+);
+
+$aliases['ia'] = array(
+	'Nuke'   => array( 'Deler_in_massa' ),
+);
+
+$aliases['id'] = array(
+	'Nuke'   => array( 'Nuklir' ),
+);
+
+$aliases['it'] = array(
+	'Nuke'   => array( 'CancellazioneMassiva' ),
+);
+
+$aliases['ja'] = array(
+	'Nuke'   => array( 'まとめて削除' ),
+);
+
+$aliases['ko'] = array(
+	'Nuke'   => array( '문서대량삭제' ),
+);
+
+$aliases['ksh'] = array(
+	'Nuke'   => array( 'Maßesch_Sigge_Fottschmiiße' ),
+);
+
+$aliases['lad'] = array(
+	'Nuke'   => array( 'Bombardea' ),
+);
+
+$aliases['lb'] = array(
+	'Nuke'   => array( 'Masseläschung' ),
+);
+
+$aliases['mk'] = array(
+	'Nuke'   => array( 'Нукирање' ),
+);
+
+$aliases['ml'] = array(
+	'Nuke'   => array( 'സർവ്വനാശം' ),
+);
+
+$aliases['nds-nl'] = array(
+	'Nuke'   => array( 'Massaal_vortdoon' ),
+);
+
+$aliases['nl'] = array(
+	'Nuke'   => array( 'MassaalVerwijderen' ),
+);
+
+$aliases['nn'] = array(
+	'Nuke'   => array( 'Massesletting' ),
+);
+
+$aliases['no'] = array(
+	'Nuke'   => array( 'Massesletting' ),
+);
+
+$aliases['pl'] = array(
+	'Nuke'   => array( 'Masowe_usuwanie' ),
+);
+
+$aliases['sa'] = array(
+	'Nuke'   => array( 'न्यूक' ),
+);
+
+$aliases['sk'] = array(
+	'Nuke'   => array( 'Zničenie' ),
+);
+
+$aliases['sv'] = array(
+	'Nuke'   => array( 'Massradering' ),
+);
+
+$aliases['tl'] = array(
+	'Nuke'   => array( 'Malawakang pagbura' ),
+);
+
+$aliases['tr'] = array(
+	'Nuke'   => array( 'Uçur' ),
+);
+
+$aliases['zh-hant'] = array(
+	'Nuke'   => array( '大量刪除' ),
+);
