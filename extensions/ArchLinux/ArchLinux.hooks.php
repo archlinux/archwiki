f3368ec1dbabe3ca1d25a98478a2fd62e3be8eab
Pierre Schmitz <pierre@archlinux.de>
2017-08-28 11:33:26 +0200

    Use MediaWiki's ConfigRegistry

--- extensions/ArchLinux/ArchLinux.hooks.php
+++ extensions/ArchLinux/ArchLinux.hooks.php
@@ -2,6 +2,8 @@
 
 namespace MediaWiki\Extensions\ArchLinux;
 
+use MediaWiki\MediaWikiServices;
+
 class Hooks
 {
     public static function onBeforePageDisplay(\OutputPage &$out, \Skin &$skin)
@@ -11,6 +13,12 @@ class Hooks
 
     public static function onSkinTemplateOutputPageBeforeExec(\SkinTemplate $skinTemplate, \QuickTemplate $tpl)
     {
+        $config = MediaWikiServices::getInstance()->getConfigFactory()->makeConfig('archlinux');
+        $archNavBar = $config->get("ArchNavBar");
+        $archHome = $config->get("ArchHome");
+        $archNavBarSelected = $config->get("ArchNavBarSelected");
+        $archNavBarSelectedDefault = $config->get("ArchNavBarSelectedDefault");
+
         ob_start();
         include __DIR__ . '/ArchNavBar.php';
         $tpl->set('headelement', $tpl->get('headelement') . ob_get_clean());
--- extensions/ArchLinux/ArchNavBar.php
+++ extensions/ArchLinux/ArchNavBar.php
@@ -2,30 +2,32 @@
 
 namespace MediaWiki\Extensions\ArchLinux;
 
-global $wgArchNavBar, $wgArchHome, $wgArchNavBarSelected, $wgArchNavBarSelectedDefault;
-
+/**
+ * @var \SkinTemplate $skinTemplate
+ * @var array $archNavBar
+ * @var string $archHome
+ * @var array $archNavBarSelected
+ * @var string $archNavBarSelectedDefault
+ */
 ?>
-<div id="archnavbar" class="noprint"><!-- Arch Linux global navigation bar -->
+<div id="archnavbar" class="noprint">
     <div id="archnavbarlogo">
-        <p><a id="logo" href="<?php if (isset($wgArchHome)) {
-                echo $wgArchHome;
-            } ?>"></a></p>
+        <p><a id="logo" href="<?= $archHome ?>"></a></p>
     </div>
     <div id="archnavbarmenu">
         <ul id="archnavbarlist">
             <?php
-            if (isset($wgArchNavBar)) {
-                foreach ($wgArchNavBar as $name => $url) {
-                    if ((isset($wgArchNavBarSelected) && $this->data['title'] == $name && in_array($name, $wgArchNavBarSelected))
-                        || (!(isset($wgArchNavBarSelected) && in_array($this->data['title'], $wgArchNavBarSelected)) && isset($wgArchNavBarSelectedDefault) && $name == $wgArchNavBarSelectedDefault)) {
-                        $anbClass = ' class="anb-selected"';
-                    } else {
-                        $anbClass = '';
-                    }
-                    echo '<li id="anb-' . strtolower($name) . '"' . $anbClass . '><a href="' . $url . '">' . $name . '</a></li>';
+            foreach ($archNavBar as $name => $url) {
+                if (($skinTemplate->getTitle() == $name && in_array($name, $archNavBarSelected))
+                    || (!(in_array($skinTemplate->getTitle(), $archNavBarSelected)) && $name == $archNavBarSelectedDefault)) {
+                    $anbClass = ' class="anb-selected"';
+                } else {
+                    $anbClass = '';
                 }
+                ?>
+            <li id="anb-<?= strtolower($name) ?>"<?= $anbClass ?>><a href="<?= $url ?>"><?= $name ?></a></li><?php
             }
             ?>
         </ul>
     </div>
-</div><!-- #archnavbar -->
+</div>
