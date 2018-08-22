7b433cb861ef97353b908089fd563e457136c7d9
Jakub Klinkovský <j.l.k@gmx.com>
2018-08-22 22:17:39 +0200

    Finish migration to HTML5-style link fragments

--- LocalSettings.archlinux.org.php
+++ LocalSettings.archlinux.org.php
@@ -86,12 +86,8 @@ $wgNamespacesWithSubpages[NS_MAIN] = true;
 $wgAllowUserJs = true;
 $wgAllowUserCss = true;
 
-# add secondary HTML5-style anchors for sections
-$wgFragmentMode = [ 'legacy', 'html5' ];
-# TODO: migrate to HTML5-style primary anchors
-# (can be done in about a week when the HTML cache is naturally renewed,
-# (at least most of the cachced pages) or we can manually purge all pages)
-//$wgFragmentMode = [ 'html5', 'legacy' ];
+# Section anchors: use HTML5-style anchors as primary, legacy as fallbback
+$wgFragmentMode = [ 'html5', 'legacy' ];
 
 
 ##
