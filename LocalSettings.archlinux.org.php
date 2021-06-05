0ea1cabebc95c43da113874b0c852f8e553e4e49
Jakub Klinkovský <j.l.k@gmx.com>
2021-06-05 09:30:16 +0200

    Allow to change the page language

--- LocalSettings.archlinux.org.php
+++ LocalSettings.archlinux.org.php
@@ -40,6 +40,10 @@ $wgLocaltimezone = 'UTC';
 # Site language code, should be one of the list in ./languages/Names.php
 $wgLanguageCode = "en";
 
+# Allow to change the page language
+$wgPageLanguageUseDB = true;
+$wgGroupPermissions['sysop']['pagelang'] = true;
+
 ## The URL base path to the directory containing the wiki;
 ## defaults for all runtime URL paths are based off of this.
 ## For more information on customizing the URLs please see:
