87518ecc8d06a18b484d8a5d02f17d48e6c60a2f
killertofus <cinemassacres@gmail.com>
2022-10-12 17:50:48 +0000

    added the exstention to make it have a dark mode

---
+++ extensions/DarkMode/Gruntfile.js
@@ -0,0 +1,35 @@
+/* eslint-env node */
+module.exports = function ( grunt ) {
+	const conf = grunt.file.readJSON( 'extension.json' );
+
+	grunt.loadNpmTasks( 'grunt-banana-checker' );
+	grunt.loadNpmTasks( 'grunt-eslint' );
+	grunt.loadNpmTasks( 'grunt-stylelint' );
+
+	grunt.initConfig( {
+		eslint: {
+			options: {
+				cache: true
+			},
+			all: [
+				'**/*.{js,json}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		stylelint: {
+			options: {
+				syntax: 'less'
+			},
+			all: [
+				'**/*.{css,less}',
+				'!node_modules/**',
+				'!vendor/**'
+			]
+		},
+		banana: conf.MessagesDirs
+	} );
+
+	grunt.registerTask( 'test', [ 'eslint', 'banana', 'stylelint' ] );
+	grunt.registerTask( 'default', 'test' );
+};
---
+++ extensions/DarkMode/README.md
@@ -0,0 +1,7 @@
+DarkMode
+========
+
+A MediaWiki extension to add a toggleable dark mode for the MediaWiki user interface.
+
+https://www.mediawiki.org/wiki/Extension:DarkMode
+
